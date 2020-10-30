c6db1282067257a034ad3f3e96b4d6b101943a1b
Arvind Raj T <46020497+Arvind-raj06@users.noreply.github.com>
2020-10-30 23:12:36 +0530

    Update GTI_615.py

--- GTI 615/GTI_615.py
+++ GTI 615/GTI_615.py
@@ -1,1 +1,23 @@
+x=list(map(int,input().split()))
+y=int(input())
+z=[]
+for a in range(x[0]):
+  f=list(map(str,input().split()))
+  z.append(f)
+k=[]
+p=int(input())
+for a in range(y-1,x[1]-y+1):
+  k.append(z[y-1][a])
 
+for a in range(y,x[0]-y+1):
+  k.append(z[a][x[1]-y])
+if y-1 !=x[0]-y:
+  for a in range(x[1]-y-1,y-2,-1):
+    k.append(z[x[0]-y][a])
+if y-1 != x[1]-y:
+  for a in range(x[0]-y-1,y-1,-1):
+    k.append(z[a][y-1])
+
+for a in k:
+  if chr(ord(a)+p).isalpha():
+    print(chr(ord(a)+p),end="")
