5dada0629bb8c49945bc214fe4f52707e1722494
Arvind Raj T <46020497+Arvind-raj06@users.noreply.github.com>
2020-10-30 23:13:09 +0530

    Update GTI_725.py
    Answer added

--- GTI 725/GTI_725.py
+++ GTI 725/GTI_725.py
@@ -1,1 +1,12 @@
+x=input().strip()
+y=input().strip()
+a=input().strip()[::-1]
+b=input().strip()[::-1]
+f=min(len(a),len(b),len(x),len(y))
+minlen = min(len(a),len(b))
+ninlen = min(len(x),len(y))
+res = "".join(["".join(x+y for x,y in zip(x,y)),x[ninlen:],y[ninlen:]])
+res1 = "".join(["".join(x+y for x,y in zip(a,b)),a[minlen:],b[minlen:]])
+res=res+res1
 
+print(res[-f:]+res[:len(res)-f])
