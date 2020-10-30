cb11f964cbb371497a915e489452cf6ad84fb02a
Arvind Raj T <46020497+Arvind-raj06@users.noreply.github.com>
2020-10-30 23:12:04 +0530

    Update GTI_531.py
    Answer add

--- GTI 531/GTI_531.py
+++ GTI 531/GTI_531.py
@@ -1,1 +1,19 @@
-
+x=int(input())
+z=[]
+for a in range(x):
+  y=list(map(str,input().split()))
+  z.append(y)
+p=list(map(str,input().split(',')))
+for a in z:
+  if p[0][1]=='+':
+    a[1]=int(a[1])+int(p[0][2:])
+  elif p[0][1]=='-':
+    a[1]=int(a[1])-int(p[0][2:])
+  elif p[0][1]=='/':
+    a[1]=int(a[1])/int(p[0][2:])
+  else:
+    a[1]=int(a[1])*int(p[0][2:])
+    
+z=sorted(z, key=lambda x:x[1])
+for a in range(int(p[1])):
+  print(z[a][0],end="")
